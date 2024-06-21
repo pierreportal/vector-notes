b1ef5497e49b37f3fa3c3ac2ce718e60f4e4f45f
pierreportal <pierreportal.mac@gmail.com>
2024-06-21 10:30:26 +0200

    tf-idf

--- research/venv/bin/activate.fish
+++ research/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/pierreportal/dev/vectornotes/venv"
+set -gx VIRTUAL_ENV "/Users/pierreportal/dev/vectornotes/research/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
