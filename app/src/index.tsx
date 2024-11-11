8ebc1960011eaa02a16c8f12a4780181e789e1b7
pierreportal <pierreportal.mac@gmail.com>
2024-11-11 19:01:11 +0100

    feat: use gql

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clxja94zr00ts07wbm6vnpd7i/master',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
