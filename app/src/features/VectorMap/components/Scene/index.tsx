72b5106267954872091dd284e65af2ec3ec3c924
pierreportal <pierreportal.mac@gmail.com>
2024-11-13 14:04:18 +0100

    fix: orbit control

--- app/src/features/VectorMap/components/Scene/index.tsx
+++ app/src/features/VectorMap/components/Scene/index.tsx
@@ -4,15 +4,20 @@ import { OrbitControls } from '@react-three/drei'
 import { Root } from '@react-three/uikit';
 
 const Rooot = Root as any
-export const Scene = ({ children }: any) => (
-    <Canvas>
-        <fog attach="fog" args={['#1d1b24', 0, 100]} />
-        <Rooot flexDirection="row">
-            <OrbitControls />
-            <ambientLight intensity={1} />
-            <Suspense fallback={null}>
-                {children}
-            </Suspense>
-        </Rooot>
-    </Canvas>
-)
+export const Scene = ({ children }: any) => {
+    return (
+        <Canvas>
+            <fog attach="fog" args={['#1d1b24', 0, 100]} />
+            <Rooot flexDirection="row">
+                <OrbitControls
+                    minDistance={20}
+                    maxDistance={40}
+                />
+                <ambientLight intensity={1} />
+                <Suspense fallback={null}>
+                    {children}
+                </Suspense>
+            </Rooot>
+        </Canvas>
+    )
+}
